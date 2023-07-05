427dcd36424ccfe76598560dd4375ccfa0417dd5
pca221 <anektanasup.pun.muids@gmail.com>
2023-07-06 00:39:28 +0700

    ecs quit game

--- Assets/Script/StartScreen/StartScreen.cs
+++ Assets/Script/StartScreen/StartScreen.cs
@@ -12,24 +12,32 @@ public class StartScreen : MonoBehaviour
 
     public GameObject easyBtn;
     public GameObject hardBtn;
+    public GameObject submenuBg,quitTxt,quitYes,quitNo,submenuHighlight;
     public static Boolean isEasyMode = true;
+    public static Boolean showQuitMenu = false;
+    public static Boolean isCoroutineRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
         if(isEasyMode) changeToEasy();
         else changeToHard();
+        hideQuitConfirm();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && !isCoroutineRunning)
+        {
+            StartCoroutine(toggleSubMenuCoroutine());
+        }
     }
 
     public void startGame(){
         SceneManager.LoadScene("Assets/Scenes/Game.unity");
     }
+
     public void changeToHard(){
         hardBtn.SetActive(true);
         easyBtn.SetActive(false);
@@ -52,5 +60,46 @@ public class StartScreen : MonoBehaviour
 
     public void loadAbout(){
         SceneManager.LoadScene("Assets/Scenes/About.unity", LoadSceneMode.Additive);
+    }
+
+    public void exit(){
+        Application.Quit();
+    }
+
+    private System.Collections.IEnumerator toggleSubMenuCoroutine()
+    {
+        isCoroutineRunning = true;
+
+        if (showQuitMenu)
+        {
+            hideQuitConfirm();
+        }
+        else
+        {
+            showQuitConfirm();
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        showQuitMenu = !showQuitMenu;
+
+        isCoroutineRunning = false;
+    }
+
+    public void hideQuitConfirm() {
+        Debug.Log("hideQuitConfirm");
+        submenuHighlight.SetActive(false);
+        submenuBg.SetActive(false);
+        quitTxt.SetActive(false);
+        quitYes.SetActive(false);
+        quitNo.SetActive(false);
+    }
+    public void showQuitConfirm() {
+        Debug.Log("showQuitConfirm");
+        submenuHighlight.SetActive(true);
+        submenuBg.SetActive(true);
+        quitTxt.SetActive(true);
+        quitYes.SetActive(true);
+        quitNo.SetActive(true);
     }
 }
